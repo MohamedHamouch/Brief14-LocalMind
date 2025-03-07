ca865439904902708e6a68ff4721d32d97441777
MohamedHamouch <med10hamouch@gmail.com>
2025-03-07 14:11:55 +0000

    set routes for questions actions

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,16 @@ use App\Http\Controllers\AnswerController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
+Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
+Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
+Route::get('/questions/{id}', [QuestionController::class, 'show'])->name('questions.show');
+
+
+Route::get('/debug', function () {
+    dd(auth()->user());
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/questions', [QuestionController::class, 'index'])->name('questions.index');
-    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
-    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
-    Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
-
-    Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
-});
-
 
 require __DIR__ . '/auth.php';
